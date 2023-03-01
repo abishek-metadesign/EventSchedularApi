2a570206f40c1917b2ca5e235ecafd2ba00966c9
abishek <abishek.dulal@metadesignsolutions.co.uk>
2023-03-01 11:51:48 +0545

    fixed the event

--- src/main/java/uk/co/metadesignsolutions/javachallenge/models/Event.java
+++ src/main/java/uk/co/metadesignsolutions/javachallenge/models/Event.java
@@ -1,37 +1,39 @@
 package uk.co.metadesignsolutions.javachallenge.models;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import uk.co.metadesignsolutions.javachallenge.enums.TimePeriod;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "event")
+@EqualsAndHashCode
+@Table(name="events")
 public class Event {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
-    private Double priority;
-
     private LocalDate startDate;
 
     private LocalDate endDate;
 
-    private TimePeriod timePeriod;
-
     private LocalDate scheduledDate;
 
-    private String startTime;
+    private LocalTime startTime;
 
-    private String endTime;
+    private LocalTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    private TimePeriod timePeriod;
+
 
 
 }
